de5efed1131a1b41c7f01a2143d581b5e33512d9
Guus <guusbeltman@gmail.com>
2015-10-19 16:00:16 +0200

    added some features

--- VideoThumbnailer/VideoThumbnailerConfig.cs
+++ VideoThumbnailer/VideoThumbnailerConfig.cs
@@ -8,224 +8,322 @@ namespace VideoThumbnailer
 {
 
 
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class VideoThumbnailerConfig
+    {
+
+        private VideoThumbnailerConfigFfmpeg ffmpegField;
+
+        private VideoThumbnailerConfigSettings settingsField;
+
+        private VideoThumbnailerConfigDir[] pathsField;
+
         /// <remarks/>
-        [System.SerializableAttribute()]
-        [System.ComponentModel.DesignerCategoryAttribute("code")]
-        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
-        [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
-        public partial class VideoThumbnailerConfig
+        public VideoThumbnailerConfigFfmpeg ffmpeg
         {
-
-            private VideoThumbnailerConfigFfmpeg ffmpegField;
-
-            private VideoThumbnailerConfigSettings settingsField;
-
-            private VideoThumbnailerConfigDir[] pathsField;
-
-            /// <remarks/>
-            public VideoThumbnailerConfigFfmpeg ffmpeg
+            get
             {
-                get
-                {
-                    return this.ffmpegField;
-                }
-                set
-                {
-                    this.ffmpegField = value;
-                }
+                return this.ffmpegField;
             }
-
-            /// <remarks/>
-            public VideoThumbnailerConfigSettings settings
+            set
             {
-                get
-                {
-                    return this.settingsField;
-                }
-                set
-                {
-                    this.settingsField = value;
-                }
-            }
-
-            /// <remarks/>
-            [System.Xml.Serialization.XmlArrayItemAttribute("dir", IsNullable = false)]
-            public VideoThumbnailerConfigDir[] paths
-            {
-                get
-                {
-                    return this.pathsField;
-                }
-                set
-                {
-                    this.pathsField = value;
-                }
+                this.ffmpegField = value;
             }
         }
 
         /// <remarks/>
-        [System.SerializableAttribute()]
-        [System.ComponentModel.DesignerCategoryAttribute("code")]
-        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
-        public partial class VideoThumbnailerConfigFfmpeg
+        public VideoThumbnailerConfigSettings settings
         {
-
-            private string ffmpeg_pathField;
-
-            private string ffprobe_pathField;
-
-            /// <remarks/>
-            public string ffmpeg_path
+            get
             {
-                get
-                {
-                    return this.ffmpeg_pathField;
-                }
-                set
-                {
-                    this.ffmpeg_pathField = value;
-                }
+                return this.settingsField;
             }
-
-            /// <remarks/>
-            public string ffprobe_path
+            set
             {
-                get
-                {
-                    return this.ffprobe_pathField;
-                }
-                set
-                {
-                    this.ffprobe_pathField = value;
-                }
+                this.settingsField = value;
             }
         }
 
         /// <remarks/>
-        [System.SerializableAttribute()]
-        [System.ComponentModel.DesignerCategoryAttribute("code")]
-        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
-        public partial class VideoThumbnailerConfigSettings
+        [System.Xml.Serialization.XmlArrayItemAttribute("dir", IsNullable = false)]
+        public VideoThumbnailerConfigDir[] paths
         {
-
-            private byte thumbcountField;
-
-            private byte thumbqualityField;
-
-            private bool info_json_generationField;
-
-            private bool resursiveField;
-
-            /// <remarks/>
-            [System.Xml.Serialization.XmlAttributeAttribute()]
-            public byte thumbcount
+            get
             {
-                get
-                {
-                    return this.thumbcountField;
-                }
-                set
-                {
-                    this.thumbcountField = value;
-                }
+                return this.pathsField;
             }
-
-            /// <remarks/>
-            [System.Xml.Serialization.XmlAttributeAttribute()]
-            public byte thumbquality
+            set
             {
-                get
-                {
-                    return this.thumbqualityField;
-                }
-                set
-                {
-                    this.thumbqualityField = value;
-                }
+                this.pathsField = value;
             }
+        }
+    }
 
-            /// <remarks/>
-            [System.Xml.Serialization.XmlAttributeAttribute()]
-            public bool info_json_generation
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class VideoThumbnailerConfigFfmpeg
+    {
+
+        private string ffmpeg_pathField;
+
+        private string ffprobe_pathField;
+
+        /// <remarks/>
+        public string ffmpeg_path
+        {
+            get
             {
-                get
-                {
-                    return this.info_json_generationField;
-                }
-                set
-                {
-                    this.info_json_generationField = value;
-                }
+                return this.ffmpeg_pathField;
             }
-
-            /// <remarks/>
-            [System.Xml.Serialization.XmlAttributeAttribute()]
-            public bool resursive
+            set
             {
-                get
-                {
-                    return this.resursiveField;
-                }
-                set
-                {
-                    this.resursiveField = value;
-                }
+                this.ffmpeg_pathField = value;
             }
         }
 
         /// <remarks/>
-        [System.SerializableAttribute()]
-        [System.ComponentModel.DesignerCategoryAttribute("code")]
-        [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
-        public partial class VideoThumbnailerConfigDir
+        public string ffprobe_path
         {
-
-            private bool resursiveField;
-
-            private bool resursiveFieldSpecified;
-
-            private string valueField;
-
-            /// <remarks/>
-            [System.Xml.Serialization.XmlAttributeAttribute()]
-            public bool resursive
+            get
             {
-                get
-                {
-                    return this.resursiveField;
-                }
-                set
-                {
-                    this.resursiveField = value;
-                }
+                return this.ffprobe_pathField;
             }
-
-            /// <remarks/>
-            [System.Xml.Serialization.XmlIgnoreAttribute()]
-            public bool resursiveSpecified
+            set
             {
-                get
-                {
-                    return this.resursiveFieldSpecified;
-                }
-                set
-                {
-                    this.resursiveFieldSpecified = value;
-                }
-            }
-
-            /// <remarks/>
-            [System.Xml.Serialization.XmlTextAttribute()]
-            public string Value
-            {
-                get
-                {
-                    return this.valueField;
-                }
-                set
-                {
-                    this.valueField = value;
-                }
+                this.ffprobe_pathField = value;
             }
         }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class VideoThumbnailerConfigSettings
+    {
+
+        private byte thumbcountField;
+
+        private byte thumbqualityField;
+
+        private bool info_json_generationField;
+
+        private string subdirField;
+
+        private bool remove_old_firstField;
+
+        private byte first_thumbnail_secField;
+
+        private byte last_thumbnail_secField;
+
+        private bool resursiveField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte thumbcount
+        {
+            get
+            {
+                return this.thumbcountField;
+            }
+            set
+            {
+                this.thumbcountField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte thumbquality
+        {
+            get
+            {
+                return this.thumbqualityField;
+            }
+            set
+            {
+                this.thumbqualityField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public bool info_json_generation
+        {
+            get
+            {
+                return this.info_json_generationField;
+            }
+            set
+            {
+                this.info_json_generationField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string subdir
+        {
+            get
+            {
+                return this.subdirField;
+            }
+            set
+            {
+                this.subdirField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public bool remove_old_first
+        {
+            get
+            {
+                return this.remove_old_firstField;
+            }
+            set
+            {
+                this.remove_old_firstField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte first_thumbnail_sec
+        {
+            get
+            {
+                return this.first_thumbnail_secField;
+            }
+            set
+            {
+                this.first_thumbnail_secField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte last_thumbnail_sec
+        {
+            get
+            {
+                return this.last_thumbnail_secField;
+            }
+            set
+            {
+                this.last_thumbnail_secField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public bool resursive
+        {
+            get
+            {
+                return this.resursiveField;
+            }
+            set
+            {
+                this.resursiveField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class VideoThumbnailerConfigDir
+    {
+
+        private bool remove_old_firstField;
+
+        private bool remove_old_firstFieldSpecified;
+
+        private bool resursiveField;
+
+        private bool resursiveFieldSpecified;
+
+        private string valueField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public bool remove_old_first
+        {
+            get
+            {
+                return this.remove_old_firstField;
+            }
+            set
+            {
+                this.remove_old_firstField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        public bool remove_old_firstSpecified
+        {
+            get
+            {
+                return this.remove_old_firstFieldSpecified;
+            }
+            set
+            {
+                this.remove_old_firstFieldSpecified = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public bool resursive
+        {
+            get
+            {
+                return this.resursiveField;
+            }
+            set
+            {
+                this.resursiveField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlIgnoreAttribute()]
+        public bool resursiveSpecified
+        {
+            get
+            {
+                return this.resursiveFieldSpecified;
+            }
+            set
+            {
+                this.resursiveFieldSpecified = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTextAttribute()]
+        public string Value
+        {
+            get
+            {
+                return this.valueField;
+            }
+            set
+            {
+                this.valueField = value;
+            }
+        }
+    }
+
+
 }
